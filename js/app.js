21725337b03d8800d6ada6b9b26efd54cb09534e
Amandine Dupays <amandine.dupays@gmail.com>
2014-09-11 16:18:14 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ var handleError  = function(error){
 //And initializes access privileges for new guests
 var initAccessPrivileges = function(object) {
   //List of accesspoint must be here
-  var accessPrivileges = privileges;
+  var accessPrivileges = [];
   var currentPrivileges = []
   if (object.access_privileges && object.access_privileges.length > 0 )
     angular.copy(object.access_privileges, currentPrivileges);
